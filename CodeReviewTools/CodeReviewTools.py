654cec04ba9eff2f29f38c732608fe58ea619268
Sergey Setkov <s.setkov@gmail.com>
2018-11-15 22:11:25 +0300

    add configuration

--- CodeReviewTools/CodeReviewTools.py
+++ CodeReviewTools/CodeReviewTools.py
@@ -1,1 +1,13 @@
 
+import TfsApi
+import json
+
+
+configPath = "CodeReviewTools.json"
+
+
+# read configuration file
+with open(configPath, 'r') as configFile:
+    config = json.load(configFile)
+
+
